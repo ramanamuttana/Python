17fbb040e37d74ba16fa424c209e0b37c665989e
ramanamuttana <ramanamuttana@gmail.com>
2021-06-12 11:44:47 +0200

    Update hello.py
    first python script from oreilly python

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
+import sys # Load a library module
 import os
+
+# A first Python script
 print(os.getcwd())
 print('Hello world ')
 print(2+10)
@@ -8,5 +11,8 @@ print('spam'*5)
 for x in 'spam':
     print(x)
 
-    
+print(sys.platform)
+print(2 ** 100)  # Raise 2 to a power
+x = 'Spam!'
+print(x * 8)
 
